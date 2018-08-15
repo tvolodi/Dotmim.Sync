4e65b21f2c2ddf23e04f85d2809a1ddcd1d5cb6b
Volodymyr Tytenko <tvolodi@hotmail.com>
2018-08-15 09:43:12 +0600

    Added new project with working GUI (at the early stage still)

--- Samples/XamarinAndroidSyncSample/MainActivity.cs
+++ Samples/XamarinAndroidSyncSample/MainActivity.cs
@@ -21,8 +21,6 @@ namespace XamarinAndroidSyncSample
 		    Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
-		    FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
 	    }
 
 	    public override bool OnCreateOptionsMenu(IMenu menu)
---
+++ XamAndroidSyncSample/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+
+namespace XamAndroidSyncSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity 
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            ListView syncDataListView = FindViewById<ListView>(Resource.Id.syncDataListView);
+            string[] syncDataArray = new string[] { "test", "test2" };
+            ArrayAdapter<string> syncDataAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, syncDataArray);
+            // ListAdapter = new ArrayAdapter<string>(this, Resource.Id.list_item, syncDataArray);
+            syncDataListView.Adapter = syncDataAdapter;
+
+            //GridView gridView = (GridView)FindViewById(Resource.Id.syncGridView);
+
+        }
+    }
+}
+
